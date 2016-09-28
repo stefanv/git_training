bf870b0ecedd2930291c75019289c34279a372ae
Alvin <alvn.zng@gmail.com>
2016-09-28 14:21:09 -0700

    fixed test_calc, add() test was in pow() test

--- calc/tests/test_calc.py
+++ calc/tests/test_calc.py
@@ -4,6 +4,7 @@ from calc import pow
 def test_add():
     """Tests add() function in calc library"""
     assert add(1, 2) == 3
+    assert add(-1, 1) == 0
 
 def test_pow():
     assert pow(1,2) == 1
@@ -11,5 +12,4 @@ def test_pow():
     assert pow(10,0) == 1
     assert pow(0.5,2) == 0.25 
     assert pow(4,0.5) == 2
-    assert add(-1, 1) == 0
 
